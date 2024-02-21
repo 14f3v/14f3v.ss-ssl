35bc917ac02355ec6bdb43c7500d8836f4be64da
contain3rlab <svnps.rnd.team@gmail.com>
2024-02-21 13:30:13 +0700

    change domain to test mockuo

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+openssl req -newkey rsa:2048 -nodes -keyout ./selfsign-ssl/privatekey.pem -x509 -days 365 -out ./selfsign-ssl/certificate.pem -subj "/CN=*.test.com"
+-addext "subjectAltName=DNS:*.test.com" \
+-addext "basicConstraints=critical,CA:TRUE,pathlen:0" \
+-addext "keyUsage=critical,keyCertSign,cRLSign,digitalSignature"
+
+openssl verify -CAfile ./selfsign-ssl/certificate.pem - verify_hostname '*.test.com' ./selfsign-ssl/certificate.pem
